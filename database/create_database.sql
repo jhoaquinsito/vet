eaca8664782a882bb42b79184b5bd4f535ce570a
Tomás Domé <dometj@gmail.com>
2015-09-13 00:20:19 -0300

    Agrego scripts para crear rol y base de datos.

---
+++ database/create_database.sql
@@ -0,0 +1,11 @@
+-- Database: vet
+
+-- DROP DATABASE vet;
+
+CREATE DATABASE vet
+  WITH OWNER = vet
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8'
+       CONNECTION LIMIT = -1;
