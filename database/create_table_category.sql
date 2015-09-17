2f78735c3b4685e7ffeb7d928ffab1fd6e29888a
Tomás Domé <dometj@gmail.com>
2015-09-16 21:11:57 -0300

    Scripts para creación de tablas, category, measure unit y presentation.

---
+++ database/create_table_category.sql
@@ -0,0 +1,15 @@
+﻿-- Table: category
+
+-- DROP TABLE category;
+
+CREATE TABLE category
+(
+  id serial NOT NULL,
+  name character varying(30) NOT NULL,
+  CONSTRAINT pk_category PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE category
+  OWNER TO vet;
