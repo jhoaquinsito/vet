a34aeb01c44f4a50dff85be364d7a02d38953e18
Tomás Domé <dometj@gmail.com>
2015-09-16 22:27:12 -0300

    Agrego script de creación de tabla manufacturer.

---
+++ database/create_table_manufacturer.sql
@@ -0,0 +1,15 @@
+﻿-- Table: manufacturer
+
+-- DROP TABLE manufacturer;
+
+CREATE TABLE manufacturer
+(
+  id serial NOT NULL,
+  name character varying(30) NOT NULL,
+  CONSTRAINT pk_manufacturer PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE manufacturer
+  OWNER TO vet;
