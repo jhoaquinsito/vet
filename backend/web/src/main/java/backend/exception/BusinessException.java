bb0e2afe7553fc8f2398cb329b90edc13ee96954
Tomás Domé <dometj@gmail.com>
2015-10-04 19:22:05 -0300

    Agrego método que permite obtener el stacktrace de un BusinessException como un string.

--- backend/web/src/main/java/backend/exception/BusinessException.java
+++ backend/web/src/main/java/backend/exception/BusinessException.java
@@ -1,28 +1,45 @@
 package backend.exception;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 /**
- * Un <code>BusinessException</code> es una <strong>EXCEPCION</strong>, que sirve para encpsular los 
- * casos donde el flujo normal no puede seguir adelante, o bien, que suceda un flujo realmente de excepcion.
- * <br>
+ * Un <code>BusinessException</code> es una <strong>EXCEPCION</strong>, que
+ * sirve para encpsular los casos donde el flujo normal no puede seguir
+ * adelante, o bien, que suceda un flujo realmente de excepcion. <br>
  *
- * Su mision es encapsular las excepciones controladas y no controladas, en cualquiera de las distintas
- * capas de la arquitectura, y comunicarla mediante el throw de ella misma, hasta llegar al RestController
- * pertinente, donde será serializada via la clase BusinessExceptionDTO
+ * Su mision es encapsular las excepciones controladas y no controladas, en
+ * cualquiera de las distintas capas de la arquitectura, y comunicarla mediante
+ * el throw de ella misma, hasta llegar al RestController pertinente, donde será
+ * serializada via la clase BusinessExceptionDTO
  * 
  * @author genesis
  *
  */
-public class BusinessException extends Exception{
-	
+public class BusinessException extends Exception {
+
 	private static final long serialVersionUID = -3332292346834265371L;
-    
-	
+
 	public BusinessException(String pMessage, Exception pInnerException) {
-		super(pMessage,pInnerException);
+		super(pMessage, pInnerException);
 	}
 
 	public BusinessException(String pMessage) {
 		super(pMessage);
+	}
+
+	/**
+	 * Método que devuelve el stack trace completo de la excepción en formato de
+	 * string.
+	 * 
+	 * @return stack trace en formato string
+	 */
+	public String getStackTraceString() {
+		StringWriter mStackTrace = new StringWriter();
+
+		this.printStackTrace(new PrintWriter(mStackTrace));
+
+		return mStackTrace.toString();
 	}
 
 }
