60f9b46e35020ecfc85f510be919fe3a867f2c46
Tomás Domé <dometj@gmail.com>
2015-10-10 03:04:27 -0300

    Agrego dbunit a los tests de integración para setear un estado en la base de datos y poder checkear resultados con dichos datos. pom.xml: agrego dbunit y libreria para asserts de JSONs.

--- backend/web/src/test/java/integration/ApplicationRestIT.java
+++ backend/web/src/test/java/integration/ApplicationRestIT.java
@@ -19,20 +19,30 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import integration.DatabaseUtils;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 
 import junit.framework.TestCase;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+    DirtiesContextTestExecutionListener.class,
+    DbUnitTestExecutionListener.class })
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml",
 		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml" })
+@DatabaseSetup("file:src/test/resources/initial-dataset.xml")
 public abstract class ApplicationRestIT extends TestCase {
 
 	@Autowired
