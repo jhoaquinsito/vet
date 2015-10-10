2e71545a9d5c299fe04a8f4caf0d441b8671187d
Tomás Domé <dometj@gmail.com>
2015-10-10 03:25:12 -0300

    Actualizo los tests de integración con pruebas de base de datos.

--- backend/web/src/test/java/integration/ApplicationIT.java
+++ backend/web/src/test/java/integration/ApplicationIT.java
@@ -2,7 +2,13 @@ package integration;
 
 
 import org.junit.Test;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 /**
  * Clase que contiene los tests de integración de toda la aplicación. Cada test
@@ -24,6 +30,17 @@ public class ApplicationIT extends ApplicationRestIT {
 	}
 	
 	@Test
+	public void postProduct_withAllFields() throws Exception {
+		
+		String mSequenceNextValue = super.getNextValueFromSequence("product_id_seq");
+		
+		super.performPost("/product", "/integration/post_product/withallfields_request")
+				.andExpect(status().isOk())
+				.andExpect(content().string(mSequenceNextValue));
+	}
+	
+	@ExpectedDatabase(assertionMode= DatabaseAssertionMode.NON_STRICT, value="file:src/test/resources/initial-dataset.xml")
+	@Test
 	public void postProduct_withoutRequiredFields() throws Exception {
 		
 		super.performPost("/product", "/integration/post_product/withoutrequiredfields_request")
@@ -37,27 +54,32 @@ public class ApplicationIT extends ApplicationRestIT {
 				.andExpect(status().isConflict());
 	}
 	
+	@ExpectedDatabase(assertionMode= DatabaseAssertionMode.NON_STRICT, value= "file:src/test/resources/data-product1-deleted.xml")
 	@Test
 	public void deleteProduct() throws Exception {
 		// TODO verificar en la base/archivo, si elimino el producto
-		super.performDelete("/product/71")
-				.andExpect(status().isOk());
+		super.performDelete("/product/1")
+				.andExpect(status().isOk())
+				.andExpect(content().string(""));
 	}
 	
 	@Test
 	public void deleteProduct_whichNotExists() throws Exception {
-		super.performDelete("/product/1111111")
+		super.performDelete("/product/123456789")
 				.andExpect(status().isConflict());
 	}
 	
+	@ExpectedDatabase(assertionMode= DatabaseAssertionMode.NON_STRICT, value="file:src/test/resources/initial-dataset.xml")
 	@Test
 	public void getProduct() throws Exception {
-		// TODO traer el producto de la base/archivo
 		
 		// TODO comparar el producto de la base con el que trajo
+		String mExpectedJSONResponse = super.readJsonFile("/integration/get_product/productone_response");
 		
-		super.performGet("/product/72")
-				.andExpect(status().isOk());
+		super.performGet("/product/1")
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(status().isOk())
+				.andExpect(content().json(mExpectedJSONResponse));
 	}
 	
 	@Test
