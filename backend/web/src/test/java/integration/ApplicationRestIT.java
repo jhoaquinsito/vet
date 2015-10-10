22ae315837e39c3629e6ea554f93eef045c1cda3
Tomás Domé <dometj@gmail.com>
2015-10-10 03:06:52 -0300

    ApplicationRestIT.java : Elimino inserts no usados.

--- backend/web/src/test/java/integration/ApplicationRestIT.java
+++ backend/web/src/test/java/integration/ApplicationRestIT.java
@@ -1,16 +1,10 @@
 package integration;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
