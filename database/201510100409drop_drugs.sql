5ad836c8d23dab58742e8627087659597f9eb648
Tomás Domé <dometj@gmail.com>
2015-10-12 23:04:38 -0300

    Cambios para que haya una sola droga por producto.

---
+++ database/201510100409drop_drugs.sql
@@ -0,0 +1,8 @@
+﻿-- elimino tabla que relacionaba muchos a muchos
+DROP TABLE product_drugs;
+
+-- agrego campo droga al producto
+ALTER TABLE product
+  ADD COLUMN drug integer;
+ALTER TABLE product
+  ADD CONSTRAINT fk_drug_drug FOREIGN KEY (drug) REFERENCES drug (id) ON UPDATE NO ACTION ON DELETE NO ACTION;
