7dde3411afff950a7a2037d325bbfe556ed80466
Tomás Domé <dometj@gmail.com>
2015-10-18 03:15:33 -0300

    REFACTOR elimino imports que no se usan.

--- backend/web/src/main/java/backend/product/batch/Batch.java
+++ backend/web/src/main/java/backend/product/batch/Batch.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
