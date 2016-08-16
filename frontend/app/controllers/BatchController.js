3a826c43fde8174a165ba57935b5a046f5d31f37
Tomás Domé <dometj@gmail.com>
2016-08-15 21:12:24 -0300

    REFACTOR: refactor de actualizacion de lista de lotes al editar/agregar un lote.

--- frontend/app/controllers/BatchController.js
+++ frontend/app/controllers/BatchController.js
@@ -111,7 +111,7 @@ app.controller('BatchController', function($scope, $location, $rootScope, $route
         };
 
         // actualizo la lista de batches con el batch
-        $scope.form.product.batches = updateListWithBatch($scope.form.product.batches, batch);
+        $scope.form.product.batches = BatchService.updateListWithBatch($scope.form.product.batches, batch);
 
         // limpio los campos de agregar batch
         $scope.form.batch = {};
@@ -127,62 +127,6 @@ app.controller('BatchController', function($scope, $location, $rootScope, $route
         }
         return isoDate;
     }
-
-    // funcion que actualiza la lista de lotes en la pantalla Cargar lotes con el lote nuevo/modificado
-    function updateListWithBatch(listOfBatches, batch) {
-        if (batch.id != null) { // si es id del batch no es null/undefined
-            // busco batch por id (si no existe devuelve -1 )
-            var targetBatchIndex = getBatchIndexById(listOfBatches, batch.id); 
-
-            if (targetBatchIndex > -1){
-                if (listOfBatches[targetBatchIndex].isoDueDate == batch.isoDueDate){
-                    // si la fecha es igual entonces actualizo el stock y no necesito hacer mas nada
-                    listOfBatches[targetBatchIndex].stock = batch.stock;
-                    return listOfBatches;
-                } else {
-                    // si la fecha es diferente, borrar el lote de la lista
-                    listOfBatches.splice(targetBatchIndex, 1);
-                }
-            } else {
-                console.log("Error: el identificador del batch no existe en la lista de batches del producto.");
-            }        
-        } 
-
-        // busco batch por isoduedate (si no existe devuelve -1 )
-        targetBatchIndex = getBatchIndexByIsoDueDate(listOfBatches, batch.isoDueDate);
-
-        if (targetBatchIndex > -1){ // si existe ya un batch con esa isoduedate
-            // actualizo el batch con los nuevos valores
-            listOfBatches[targetBatchIndex].isoDueDate = batch.isoDueDate;
-            listOfBatches[targetBatchIndex].stock = batch.stock;
-        }
-        else { // si no existe un batch con esa isoduedate
-            // agrego el nuevo batch a la lista
-            listOfBatches.push(batch);
-        }
-
-        return listOfBatches;
-    };
-
-    // funcion que busca un lote por fecha de vencimiento en una lista de lotes
-    function getBatchIndexByIsoDueDate(listOfBatches, batchIsoDueDate) {
-        for (var i = 0; i < listOfBatches.length; i++) {
-            if (listOfBatches[i].isoDueDate === batchIsoDueDate) {
-                return i;
-            }
-        }
-        return -1;
-    };
-
-    // funcion que busca un lote por id en una lista de lotes
-    function getBatchIndexById(listOfBatches, batchId) {
-        for (var i = 0; i < listOfBatches.length; i++) {
-            if (listOfBatches[i].id === batchId) {
-                return i;
-            }
-        }
-        return -1;
-    };
 
     // funcion que se ejecuta al eliminar un lote de la lista de lotes de la pantalla Cargar lote
     $scope.removeBatchToTableAction = function(batch) {
--- frontend/app/services/BatchService.js
+++ frontend/app/services/BatchService.js
@@ -80,5 +80,61 @@ app.factory('BatchService', function() {
         return product;
     };
 
+    // funcion que actualiza la lista de lotes en la pantalla Cargar lotes con el lote nuevo/modificado
+    this.updateListWithBatch = function(listOfBatches, batch) {
+        if (batch.id != null) { // si es id del batch no es null/undefined
+            // busco batch por id (si no existe devuelve -1 )
+            var targetBatchIndex = getBatchIndexById(listOfBatches, batch.id); 
+
+            if (targetBatchIndex > -1){
+                if (listOfBatches[targetBatchIndex].isoDueDate == batch.isoDueDate){
+                    // si la fecha es igual entonces actualizo el stock y no necesito hacer mas nada
+                    listOfBatches[targetBatchIndex].stock = batch.stock;
+                    return listOfBatches;
+                } else {
+                    // si la fecha es diferente, borrar el lote de la lista
+                    listOfBatches.splice(targetBatchIndex, 1);
+                }
+            } else {
+                console.log("Error: el identificador del batch no existe en la lista de batches del producto.");
+            }        
+        } 
+
+        // busco batch por isoduedate (si no existe devuelve -1 )
+        targetBatchIndex = getBatchIndexByIsoDueDate(listOfBatches, batch.isoDueDate);
+
+        if (targetBatchIndex > -1){ // si existe ya un batch con esa isoduedate
+            // actualizo el batch con los nuevos valores
+            listOfBatches[targetBatchIndex].isoDueDate = batch.isoDueDate;
+            listOfBatches[targetBatchIndex].stock = batch.stock;
+        }
+        else { // si no existe un batch con esa isoduedate
+            // agrego el nuevo batch a la lista
+            listOfBatches.push(batch);
+        }
+
+        return listOfBatches;
+    };
+
+    // funcion que busca un lote por fecha de vencimiento en una lista de lotes
+    function getBatchIndexByIsoDueDate(listOfBatches, batchIsoDueDate) {
+        for (var i = 0; i < listOfBatches.length; i++) {
+            if (listOfBatches[i].isoDueDate === batchIsoDueDate) {
+                return i;
+            }
+        }
+        return -1;
+    };
+
+    // funcion que busca un lote por id en una lista de lotes
+    function getBatchIndexById(listOfBatches, batchId) {
+        for (var i = 0; i < listOfBatches.length; i++) {
+            if (listOfBatches[i].id === batchId) {
+                return i;
+            }
+        }
+        return -1;
+    };
+
     return this;
 });
